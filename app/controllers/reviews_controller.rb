de8ef86fc63d2d229efb28b61529178b9ee66211
adamrysyk <rysyk.adam@gmail.com>
2016-10-17 01:52:41 +0000

    reviews can now only be submitted if a user is logged in

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,8 +1,6 @@
 class ReviewsController < ApplicationController
 
-  def current_user
-   @current_user ||= User.find(session[:user_id]) if session[:user_id]
- end
+  before_filter :current_user
 
  def create
    @product = Product.find params[:product_id]
